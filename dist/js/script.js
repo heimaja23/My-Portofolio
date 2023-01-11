feb2d6a823bb419c92ceed2fe2ccd00d0841dee9
Maja <97025041+heimaja23@users.noreply.github.com>
2023-01-11 12:58:16 +0700

    update

--- dist/js/script.js
+++ dist/js/script.js
@@ -11,12 +11,25 @@ hamburger.addEventListener('click', function() {
 window.onscroll = function(){
     const header = document.querySelector('header');
     const fixedNav = header.offsetTop;
+    const pushtop = document.querySelector('#pushtop');
 
     if(window.pageYOffset > fixedNav){
         header.classList.add('navbar-fixed');
+        pushtop.classList.remove('hidden');
+        pushtop.classList.add('flex');
     } else {
         header.classList.remove('navbar-fixed');
+        pushtop.classList.remove('flex');
+        pushtop.classList.add('hidden');
     }
 };
+
+//another click
+window.addEventListener('click',function(e){
+    if(e.target != hamburger && e.target!= NavMenu){
+        hamburger.classList.remove('hamburger-active');
+        NavMenu.classList.add('hidden');
+    };
+});
 
 
